cd4412d510cdfca81ef09b81254711ec501f55c3
Arshad Hannan <owlfly360@gmail.com>
2026-04-27 21:53:23 +0530

    intial project setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "han.work.expense_tracker"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
